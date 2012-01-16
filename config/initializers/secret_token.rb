fd08e556c0213237a7492c6153d905b7492eb491
ziglee <ziglee@gmail.com>
2012-01-16 21:29:32 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoiasesporteRails::Application.config.secret_token = '1b18674846fc68497a0faa5f8237d04d96042782c7a25a98c28960ce97ec5d7fe4f5403fc78ff4346dd2bef26c41a631d44d44f090e216caf9f2a654fd528ada'
